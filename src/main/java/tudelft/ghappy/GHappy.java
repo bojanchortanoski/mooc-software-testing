5b3543fb54b8dee6fe16b8ae3e8eafc162b867cb
Bojan <bchortanoski@gmail.com>
2018-12-09 02:11:16 +0100

    gHappy tests

--- src/main/java/tudelft/ghappy/GHappy.java
+++ src/main/java/tudelft/ghappy/GHappy.java
@@ -4,10 +4,20 @@ public class GHappy {
 
     public boolean gHappy(String str) {
         assert str!=null;
+
+        if (!str.contains("g")) {
+            return false;
+        }
+
         for(int i = 0; i < str.length(); i++) {
-            if(str.charAt(i) == 'g') {
-                if (i >= 0 && str.charAt(i-1) == 'g') { continue; }
-                if (i+1 < str.length() && str.charAt(i+1) == 'g') { continue; }
+
+            if(Character.toLowerCase(str.charAt(i)) == 'g') {
+                if (i - 1 >= 0 && Character.toLowerCase(str.charAt(i - 1)) == 'g') {
+                    continue;
+                }
+                if (i+1 < str.length() && Character.toLowerCase(str.charAt(i + 1)) == 'g') {
+                    continue;
+                }
                 return false;
             }
         }
--- src/test/java/tudelft/ghappy/GHappyTest.java
+++ src/test/java/tudelft/ghappy/GHappyTest.java
@@ -1,4 +1,55 @@
 package tudelft.ghappy;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+
 public class GHappyTest {
+    private GHappy result;
+
+    @BeforeEach
+    public void init() {
+        result = new GHappy();
+    }
+
+    @Test
+    public void testWithOneLetterG() {
+        Assertions.assertFalse(result.gHappy("g"));
+    }
+
+    @Test
+    public void testWithInputOfAllGs() {
+        Assertions.assertTrue(result.gHappy("ggggg"));
+    }
+
+    @Test
+    public void testWithInputOfNoGs() {
+        Assertions.assertFalse(result.gHappy("xxxx"));
+    }
+
+    @Test
+    public void testFirstExampleGiven() {
+        Assertions.assertTrue(result.gHappy("xxggxx"));
+    }
+
+    @Test
+    public void testSecondExampleGiven() {
+        Assertions.assertFalse(result.gHappy("xxgxx"));
+    }
+
+    @Test
+    public void testThirdExampleGiven() {
+        Assertions.assertFalse(result.gHappy("xxggyygxx"));
+    }
+
+    @Test
+    public void testFirstExampleGivenUsingUpperAndLowerCaseLetters() {
+        Assertions.assertTrue(result.gHappy("xXgGxX"));
+    }
+
+    @Test
+    public void testWithEmptyString() {
+        Assertions.assertFalse(result.gHappy(""));
+    }
 }
